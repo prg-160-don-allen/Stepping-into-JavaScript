8c4aab107883c28022dd1e762c378809c9ad3410
donallen <da34773@ravens.racc.edu>
2018-02-03 16:58:08 -0500

    Changed js code to test to see if you can come in using Logical Operators.

--- test.js
+++ test.js
@@ -1,21 +1,13 @@
-var myAge = 59;
+var myAge = 26;
 
-if (myAge > 30){
+if (myAge < 18 || myAge > 30 || myAge === 25){
 	
-	document.write("you are over 30!");
+	document.write("you ain't coming!");
 	
-} else if (myAge > 20){
-		   
-	document.write("you are over 20!");
-
-} else if (myAge > 10){
-		   
-	document.write("you are over 10!");
-
 } else {
-		   
-	document.write("you are not over 10!");
+	
+	document.write("You can come, you cool dude");
+	
 }
-
 
 
