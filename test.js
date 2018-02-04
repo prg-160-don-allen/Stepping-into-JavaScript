2366734676edc414e0f14491db0b1b3844108979
donallen <da34773@ravens.racc.edu>
2018-02-03 20:16:39 -0500

    Changed js code to use a for loop to change links and changed link numbers in index.html.

--- test.js
+++ test.js
@@ -1,18 +1,8 @@
-for (i = 0; i < 10; i++) {
-	
-	if(i === 5 || i === 3){
-		
-		continue;
-		
-	}
-	
-	console.log(i);
-	
-	if(i === 7){
-		
-		break;
-	}
+var links = document.getElementsByTagName("a");
 
+for(i = 0; i < links.length; i++){
+	
+	
+	links[i].className = "link-" + 1;
+	
 }
-
-console.log("I have broken out of the loop.");
