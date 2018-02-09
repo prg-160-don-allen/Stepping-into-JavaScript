ec317623f7353f223a2cfff69b539a8d23eed5b2
donallen <da34773@ravens.racc.edu>
2018-02-09 13:07:24 -0500

    Updated test.js for Assignment 10 Functions

--- test.js
+++ test.js
@@ -1,19 +1,34 @@
-for (i =0; i < 10; i++){
+// 5 number average function
+function getAverage (a,b,c,d,e){
 	
-	if(i === 5 || i === 3){
-		
-		continue;
-		
-	}
-	
-	console.log(i);
-	
-	if(i===7){
-		
-		break;
-		
-	}
+	var average = (a + b + c + d + e) / 5;
+	return average;
 	
 }
 
-console.log("I have broken out of the loop");
+var myResult = getAverage(21,7,56,99,46);
+console.log("the average is " + myResult);
+
+
+// 2 number difference function
+function getDiff (a,b){
+	
+	var diff = (a - b);
+	return diff;
+	
+}
+
+var myDiff = getDiff(42,22);
+console.log("the difference is " + myDiff);
+
+
+// Combine 2 strings function
+function donAllen (a,b){
+	
+	var longString = (a + b);
+	return longString;
+			
+}
+
+	var myString = donAllen('Go ', 'Eagles!!');
+	console.log("What I have to say is: " + myString);
